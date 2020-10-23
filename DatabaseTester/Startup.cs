19afef79c311d21e058a98ed555cf1b11bfe6cc4
alittleton98 <andrew.littleton1998@gmail.com>
2020-10-23 16:48:45 -0500

    Created local database and test project to try connection

--- DatabaseTester/Startup.cs
+++ DatabaseTester/Startup.cs
@@ -5,12 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-//using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace eLoan_Project
+namespace DatabaseTester
 {
     public class Startup
     {
@@ -25,8 +24,6 @@ namespace eLoan_Project
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
