8208f50e7312bcd877e63176a07322a1dc6c5833
alittleton98 <andrew.littleton1998@gmail.com>
2020-10-23 17:11:04 -0500

    nuGet packages installed

---
+++ DatabaseTester/Controllers/HelloThereController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatabaseTester.Controllers
+{
+    public class HelloThereController : Controller
+    {
+
+        public string HelloThere()
+        {
+            return "General Kenobi";
+        }
+
+        public string Index()
+        {
+            return "This is the default action";
+        }
+
+
+        /*
+        public IActionResult Index()
+        {
+            return View();
+        }
+        */
+    }
+}
