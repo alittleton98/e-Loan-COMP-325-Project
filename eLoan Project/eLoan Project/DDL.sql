b1f23d4953f296f23ada599a2cd3b1b86d507665
Anshul Shanker <anshulshankar@gmail.com>
2020-12-02 15:52:03 -0600

    Update DDL.sql

--- eLoan Project/eLoan Project/DDL.sql
+++ eLoan Project/eLoan Project/DDL.sql
@@ -1,15 +1,8 @@
-set FOREIGN_KEY_CHECKS=0;
-
-DROP TABLE IF EXISTS aplication;
-DROP TABLE IF EXISTS customer;
-DROP TABLE IF EXISTS profile;
-DROP TABLE IF EXISTS address;
-DROP TABLE IF EXISTS login;
-DROP TABLE IF EXISTS bank_details;
-DROP TABLE IF EXISTS loan_details;
+-- disable all constraints
+EXEC sp_MSforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"
 
 create table application(
-  application_id        int         not null auto_increment,
+  application_id        INT         not NULL    IDENTITY    PRIMARY KEY,
   profile_id            int         not null,
   last4ssn              int         not null,
   loan_state            char(25)    not null,
@@ -20,59 +13,54 @@ create table application(
   amount_requested      Float(2)    not null,
   tenure_in_months      int         not null,
   application_date      Date        not null,
-
-
-    primary key (application_id)
 );
 
 create table customer(
-  customer_id        int    not null auto_increment,
+  customer_id        int    not null IDENTITY  PRIMARY KEY,
   application_id     int    not null,
   profile_id         int    not null,
   loan_details_id    int    not null,
-
-    primary key (customer_id)
 );
 
 create table profile(
-  profile_id    int             not null,
+  profile_id    int             not null IDENTITY  PRIMARY KEY,
   first_name    varchar(30)     not null,
   last_name     varchar(30)     not null,
   email         varchar(30)     not null,
   address_id    int             not null,
-  phone_number  int(10)         not null,
-
-    primary key(profile_id)
+  phone_number  int         not null,
 );
 
 create table login(
-  email               varchar(30)    not null,
+  email               varchar(30)    not null PRIMARY KEY,
   password            varchar(30)    not null,
   customer_id         int            not null,
-
-    primary key (email)
 );
 
 create table bank_details(
-  bank_details_id    int        not null auto_increment,
+  bank_details_id    int        not null IDENTITY  PRIMARY KEY,
   loan_amount        Float(2)   not null,
   balance            Float(2)   not null,
   interest_rate      Float(2)   not null,
   tenure             int        not null,
   loan_date          DATE       not null,
-
-    primary key (bank_details_id)
-
 );
 
 create table loan_details (
-  loan_details_id    int          not null auto_increment,
+  loan_details_id    int          not null IDENTITY  PRIMARY KEY,
   customer_id        int          not null,
   bank_name          varchar(25)  not null,
   routing_number     int          not null,
   account_number     int          not null,
+);
 
-  primary key (loan_details_id)
+create table address (
+    address_id    int   not null IDENTITY  PRIMARY KEY,
+    street        varchar not null,
+    city        varchar not null,
+    state        varchar not null,
+    zip_code        varchar not null,
+    country        varchar not null,
 );
 
 alter table application ADD foreign key (profile_id) references profile(profile_id);
